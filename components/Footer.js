7ed2ec9930334da8861623a9c69bf081007f5dff
Gokul Kathirvel <gokulkathirvel@live.com>
2021-02-13 08:24:01 +0000

    adds crosa app footer

--- components/Footer.js
+++ components/Footer.js
@@ -6,8 +6,9 @@ const { primaryColor, fontColor } = variables;
 export default function Footer() {
   return (
     <div>
-      <footer className="fixed bottom-0 w-full p-1 text-white text-center">
+      <footer className="fixed bottom-0 w-full p-2 text-white text-center text-sm">
         &copy; 2020 Chennai Web Group
+        &bull; Created and Maintained by <a className="text-white underline" href="https://bit.ly/crosaapps" target="_blank">Crosa Apps Ltd.</a>
       </footer>
 
       <style jsx>{`
