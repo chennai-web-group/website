3393fe7d8321b482a27626243af4853bd501527b
Gokul Kathirvel <gokulkathirvel@live.com>
2021-02-13 08:28:45 +0000

    updated copyright

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div>
       <footer className="fixed bottom-0 w-full p-2 text-white text-center text-sm">
-        &copy; 2020 Chennai Web Group
+        &copy; 2021 Chennai Web Group
         &bull; Created and Maintained by <a className="text-white underline" href="https://bit.ly/crosaapps" target="_blank">Crosa Apps Ltd.</a>
       </footer>
 
